b5c5ed167471e2cc95f21f31675d3f84e62ee7de
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-09 19:39:39 +0200

    Bump version number to 0.6.0

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy@0.5.0');
+  api.use('jagi:astronomy@0.6.0');
   api.use('underscore');
 
   api.imply('jagi:astronomy');
